c982b0390a61ca0cafd923567d8e51c34ee967fe
James Heywood <jdheywood@gmail.com>
2016-12-06 17:17:40 +0000

    Section 4 completed, containers, reducers, actions, action handlers etc

---
+++ src/containers/book-detail.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class BookDetail extends Component {
+  render() {
+    if (!this.props.book) {
+      return <div>Select a book to get started.</div>;
+    }
+
+    return (
+      <div>
+        <h3>Details for:</h3>
+        <div>Title: {this.props.book.title}</div>
+        <div>Pages: {this.props.book.pages}</div>
+      </div>
+    );
+  }
+}
+
+function mapStateToProps(state) {
+  // Whatever is returned from this function will end up as props on the BookList container
+  return {
+    book: state.activeBook
+  };
+}
+
+// Promote BookDetail from component to container
+export default connect(mapStateToProps)(BookDetail);
